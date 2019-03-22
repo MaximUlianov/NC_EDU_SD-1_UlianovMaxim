f3b3bb5feb089a10522a06a1369a651bdbced3de
ulianoff <unaxim2000@mail.ru>
2019-03-22 17:07:29 +0300

    start project

---
+++ subscriptions control/frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { BillingAccountsComponent } from './components/billing-accounts/billing-accounts.component';
+import {Ng4LoadingSpinnerModule} from "ng4-loading-spinner";
+import { MainPageComponent } from './components/main-page/main-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BillingAccountsComponent,
+    MainPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    Ng4LoadingSpinnerModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
